bf5fdf2c406658205b19d870c1d42bcf26ecace1
Mauro.V <remanomauro@gmail.com>
2023-02-08 06:00:52 +0100

    segment-failed

--- app/conference/speakers/page.js
+++ app/conference/speakers/page.js
@@ -1,30 +1,39 @@
+import Link from "next/link";
 
-// Static site generation by default
-async function fetchSpeakers(){
+export let speakerJson = {};
+
+// Static data fetching
+async function fetchSpeakers() {
   const response = await fetch(
-    "https://raw.githubusercontent.com/adhithiravi/Consuming-GraphqL-Apollo/master/api/data/speakers.json",
-    { next: {revalidate: 20} }
+    "https://raw.githubusercontent.com/adhithiravi/Consuming-GraphqL-Apollo/master/api/data/speakers.json"
   );
 
-  const data = response.json();
+  const data = await response.json();
+  speakerJson = data;
   return data;
 }
 
-export default async function page() {
+export default async function Page() {
   const data = await fetchSpeakers();
 
   return (
-    <div className="bg-slate-200">
-    <div className="self-start whitespace-nowrap bg-gray-700 px-3 py-1 text-sm font-medium tabular-nums text-gray-100">
-    Last Rendered: {new Date().toLocaleDateString()}
-    </div>
-    <h1 className="text-center font-bold text-2xl p-4">Welcome to Globomatics Speakers</h1>
-    {data.speakers.map(({id, name, bio}) => (
-      <div key={id} className="p-4 border-b bg-slate-300">
-        <h3 className="font-bold ml-4 p-2 underline">{name}</h3>
-        <h5 className="italic font-medium">- {bio}</h5>
+    <div className="">
+      <div className="self-start whitespace-nowrap rounded-lg bg-gray-700 px-3 py-1 text-sm font-medium tabular-nums text-gray-100">
+        Last Rendered: {new Date().toLocaleTimeString()}
       </div>
-    ))}    
+      <h1>Welcome to Globomantics Speakers</h1>
+      {data.speakers.map(({ id, name, bio }) => (
+        <div key={id} className="">
+          <Link
+            className=""
+            href={`/conference/speakers/${bio}`}
+          >
+            <h3 className="">{bio}</h3>
+          </Link>
+          <h5 className="">{bio}</h5>
+        </div>
+      ))}
     </div>
-  )
+  );
 }
+
